64d03ea51a297e17f445711845ef65506da32ff0
MikaelGrace <MikeOkoduwa700@gmail.com>
2025-03-05 11:07:25 +0100

    implement the winner function

--- js/script.js
+++ js/script.js
@@ -26,31 +26,31 @@ buttons.forEach((button) => {
     });
 });
 
-// function checkWin(player) {
-//     //check rows and columns
+function checkWin(player) {
+    //check rows and columns
 
-//     for (let i = 0; i < 3; i++) {
-//         if (board[i][0] === player && board[i][1] === player && board[i][2] === player) {
-//             return true;
-//         }
+    for (let i = 0; i < 3; i++) {
+        if (board[i][0] === player && board[i][1] === player && board[i][2] === player) {
+            return true;
+        }
 
-//         if (board[0][i] === player && board[1][i] === player && board[2][i] === player) {
-//             return true;
-//         }
+        if (board[0][i] === player && board[1][i] === player && board[2][i] === player) {
+            return true;
+        }
         
-//     }
+    }
 
-//     //check diagonals
-//     if (board[0][0] === player && board[1][1] === player && board[2][2] === player) {
-//         return true;
-//     }
+    //check diagonals
+    if (board[0][0] === player && board[1][1] === player && board[2][2] === player) {
+        return true;
+    }
 
-//     if (board[0][2] === player && board[1][1] === player && board[2][0] === player) {
-//         return true;
-//     }
+    if (board[0][2] === player && board[1][1] === player && board[2][0] === player) {
+        return true;
+    }
 
-//     return false;
-// }
+    return false;
+}
 
 // function resetBoard() {
 //     board.forEach((row, i) => {

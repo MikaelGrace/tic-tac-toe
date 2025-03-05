c41fb44d4d3144e78ea6c9f4aa129d6729f13bc8
MikaelGrace <MikeOkoduwa700@gmail.com>
2025-03-05 14:08:21 +0100

    use SVG icons for the X and O instead of text

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ const board = [
     [null, null, null],
     [null, null, null]
 ]
+const buttons = document.querySelectorAll(".gameboard button");
+const letterX = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon1"><title>alpha-x</title><path d="M9,7L11,12L9,17H11L12,14.5L13,17H15L13,12L15,7H13L12,9.5L11,7H9Z" /></svg>`;
+const letterO = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon2"><title>circle-outline</title><path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>`;
+
 
 let currentPlayer = "X";
 
-const buttons = document.querySelectorAll(".gameboard button");
 
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
@@ -14,7 +17,7 @@ buttons.forEach((button) => {
 
         if (!board[row][col]) {
             board[row][col] = currentPlayer;
-            e.target.textContent = currentPlayer;
+            e.target.innerHTML = currentPlayer === "X" ? letterX : letterO;
         }
 
         if (checkWin(currentPlayer)) {
@@ -60,7 +63,8 @@ function resetBoard() {
     });
 
     buttons.forEach((button) => {
-        button.textContent = "";
+        //button.textContent = "";
+        button.innerHTML = "";
     });
     currentPlayer = "X";
 }
